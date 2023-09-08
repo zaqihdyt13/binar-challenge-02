d3feea995387ca0b7d3d26e3fb2c8550effe0dff
zaqihdyt13 <sakiihdyt13@gmail.com>
2023-09-08 21:27:00 +0700

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+console.log("NOMOR 1 ----------");
 const no1 = require("./no1.js");
-const no2 = require("./no2.js");
-const no3 = require("./no3.js");
-const no4 = require("./no4.js");
-const no5 = require("./no5.js");
+console.log("\n");
 
-// console.log("Free Memory:", os.freemem());
+console.log("NOMOR 2 ----------");
+const no2 = require("./no2.js");
+console.log("\n");
+
+console.log("NOMOR 3 ----------");
+const no3 = require("./no3.js");
+console.log("\n");
+
+console.log("NOMOR 4 ----------");
+const no4 = require("./no4.js");
+console.log("\n");
+
+console.log("NOMOR 5 ----------");
+const no5 = require("./no5.js");
+console.log("\n");
